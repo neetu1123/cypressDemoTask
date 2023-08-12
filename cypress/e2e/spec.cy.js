040be88f91fa92560d23757026f966dd4466c4bc
Neetu <neetu.chaurasiya@vidyamantra.com>
2023-08-12 15:41:11 +0530

    updated

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,31 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('from clear', () => {
+  it('all fields should be clear', () => {
+    cy.visit('http://localhost:3001/')
+    cy.get('[data-email="email"]').type('neetu@example.com')
+    cy.get('[data-name="name"]').type('neetu')
+    cy.get('#Awsome').check()
+    cy.get('[data-clear="clear"]').click()
+    cy.get('[data-name="name"]').should('have.value', '')
+    cy.get('[data-email="email"]').should('have.value', '')
+  })
+})
+
+describe('empty form', () => {
+  it('have an error msg', () => {
+    cy.visit('http://localhost:3001/')
+    cy.get('[data-submit="submit"]').click()
+    cy.get('[data-errorMsg="errorMsg"]').should('be.visible')
+  })
+})
+
+describe('submit form', () => {
+  it('have an succuss msg', () => {
+    cy.visit('http://localhost:3001/')
+    cy.get('[data-email="email"]').type('neetu@example.com')
+    cy.get('[data-name="name"]').type('neetu')
+    cy.get('#Awsome').check()
+    cy.get('[data-submit="submit"]').click()
+    cy.get('[data-success="success"]').should('be.visible')
+
   })
 })
